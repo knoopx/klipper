c0b974a0df9eea78f1b7ebac752a4d210329559a
Victor Martinez <knoopx@gmail.com>
2019-09-28 10:02:19 +0200

    Add respond callbacks for RPC

--- klippy/gcode.py
+++ klippy/gcode.py
@@ -28,6 +28,7 @@ class GCodeParser:
         self.partial_input = ""
         self.pending_commands = []
         self.bytes_read = 0
+        self.respond_callbacks = []
         self.input_log = collections.deque([], 50)
         # Command handling
         self.is_printer_ready = False
@@ -292,7 +293,8 @@ class GCodeParser:
             self._process_commands(script.split('\n'), need_ack=False)
     def get_mutex(self):
         return self.mutex
-    # Response handling
+    def register_respond_callback(self, callback):
+        self.respond_callbacks.append(callback)
     def ack(self, msg=None):
         if not self.need_ack or self.is_fileinput:
             return
@@ -309,6 +311,8 @@ class GCodeParser:
             return
         try:
             os.write(self.fd, msg+"\n")
+            for callback in self.respond_callbacks:
+                callback(msg+"\n")
         except os.error:
             logging.exception("Write g-code response")
     def respond_info(self, msg, log=True):
