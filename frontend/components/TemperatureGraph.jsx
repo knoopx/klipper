1257c5f978af6cdf1700f578bc3164d5a4a9d214
Victor Martinez <knoopx@gmail.com>
2019-10-04 15:05:10 +0000

    wip

--- frontend/components/TemperatureGraph.jsx
+++ frontend/components/TemperatureGraph.jsx
@@ -1,48 +1,44 @@
-import React from "react";
-import { ScaleSVG } from "@vx/responsive";
-import { Group } from "@vx/group";
-import { LinePath } from "@vx/shape";
-import { curveMonotoneX } from "@vx/curve";
-import { genDateValue } from "@vx/mock-data";
-import { scaleTime, scaleLinear } from "@vx/scale";
-import { extent, max } from "d3-array";
-import { scaleOrdinal } from "@vx/scale";
-
-import Colors from "../theming/colors";
+import React from "react"
+import { ScaleSVG } from "@vx/responsive"
+import { Group } from "@vx/group"
+import { LinePath } from "@vx/shape"
+import { genDateValue } from "@vx/mock-data"
+import { scaleTime, scaleLinear, scaleOrdinal } from "@vx/scale"
+import { extent, max } from "d3-array"
 
 function genLines(num) {
   return new Array(num).fill(1).map(() => {
-    return genDateValue(25);
-  });
+    return genDateValue(25)
+  })
 }
 
-const series = genLines(3);
+const series = genLines(3)
 const data = series.reduce((rec, d) => {
-  return rec.concat(d);
-}, []);
+  return rec.concat(d)
+}, [])
 
 // accessors
-const x = d => d.date;
-const y = d => d.value;
+const x = (d) => d.date
+const y = (d) => d.value
 
 export default ({ width, height }) => {
-  const xMax = width;
-  const yMax = height;
+  const xMax = width
+  const yMax = height
 
   const xScale = scaleTime({
     range: [0, xMax],
-    domain: extent(data, x)
-  });
+    domain: extent(data, x),
+  })
 
   const yScale = scaleLinear({
     range: [yMax, 0],
-    domain: [0, max(data, y)]
-  });
+    domain: [0, max(data, y)],
+  })
 
   const colorScale = scaleOrdinal({
     domain: series,
-    range: [Colors.red, Colors.blue, Colors.green, Colors.blue]
-  });
+    range: [Colors.red, Colors.blue, Colors.green, Colors.blue],
+  })
 
   return (
     <ScaleSVG width={width} height={height}>
@@ -50,13 +46,13 @@ export default ({ width, height }) => {
         <Group key={`lines-${i}`}>
           <LinePath
             data={d}
-            x={d => xScale(x(d))}
-            y={d => yScale(y(d))}
+            x={(d) => xScale(x(d))}
+            y={(d) => yScale(y(d))}
             stroke={colorScale(i)}
             strokeWidth={1}
           />
         </Group>
       ))}
     </ScaleSVG>
-  );
-};
+  )
+}
