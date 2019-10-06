19a39da725015ec7d1cc354db8a20fc07523c73f
Victor Martinez <knoopx@gmail.com>
2019-10-06 12:51:55 +0200

    wip

--- klippy/extras/kwc.py
+++ klippy/extras/kwc.py
@@ -1,4 +1,39 @@
 import logging
+import tornado.escape
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.websocket
+import os.path
+import uuid
+import base64
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    clients = set()
+
+    def initialize(self, kwc):
+      self.kwc = kwc
+      pc = tornado.ioloop.PeriodicCallback(self.report_status, 1000)
+      pc.start()
+
+    def open(self):
+        self.clients.add(self)
+
+    def on_close(self):
+        self.clients.remove(self)
+
+    def on_message(self, message):
+        # parsed = tornado.escape.json_decode(message)
+        # body = parsed["body"]
+        pass
+
+    def report_status(self):
+        for client in self.clients:
+            try:
+                client.write_message({status: self.kwc.status})
+            except:
+                logging.error("Error sending message", exc_info=True)
+
 
 class KlipperWebControl:
     def __init__(self, config):
@@ -7,17 +42,23 @@ class KlipperWebControl:
         self.status_timer = reactor.register_timer(self.update_status_callback)
         self.get_statuses = []
         self.printer.register_event_handler("klippy:ready", self.handle_ready)
-        self.status = {}
+        self.status = dict()
+        self.app = tornado.web.Application([
+            # (r"/", MainHandler),
+            (r"/ws", WebSocketHandler, {"kwc": self})
+        ], cookie_secret=base64.b64encode(uuid.uuid4().bytes + uuid.uuid4().bytes))
 
     def handle_ready(self):
         self.get_statuses = [(name, o.get_status) for name, o in self.printer.lookup_objects() if hasattr(o, 'get_status')]
         reactor = self.printer.get_reactor()
         reactor.update_timer(self.status_timer, reactor.NOW)
 
+        self.app.listen(9090)
+        tornado.ioloop.IOLoop.current().start()
+
     def update_status_callback(self, eventtime):
         for name, get_status in self.get_statuses:
             self.status[name] = get_status(eventtime)
-
         return eventtime + 1.
 
 def load_config(config):
